e141a0804b57e51e2bc1c4ebed0b03ff8cfd5e27
david220wang385 <david220wang385@gmail.com>
2020-07-11 19:28:17 -0400

    changes

--- key_seq_gen.py
+++ key_seq_gen.py
@@ -1,10 +1,26 @@
+import hashlib
+from hashlib import md5, sha3_384
+"""
+References:
+https://www.geeksforgeeks.org/hashlib-sha3_384-in-python/
+https://www.tutorialspoint.com/sha-in-python
+
+"""
+
+def cipher(message, key):
+     return bytes([message[i] ^ key[i % len(key)] for i in range(0, len(message))])
+
 key = "tiger"
-dictionary = 'abcdefghijklmnopqrstuvwxyz'
+msg = "nobody expects the spammish repitition"
 
-# Get all two character pairs
-pairs = [key[i]+key[i+1] for i in range(len(key)-1)]
-print(pairs)
+# Hash key to create keystream and XOR to encrypt
+# Using hashlib.sha3_384() method 
+keystream = sha3_384() 
+keystream.update(key.encode())
 
-# Assume key is lowercase
-for pair in pairs:
-    (ord(pair[0]) - 97) + (ord(pair[1]) - 97)
+print(keystream.hexdigest().encode())
+
+ciphertext = cipher(msg.encode(), key.encode())
+print(ciphertext)
+cipher_read = bytes(ciphertext)
+print(cipher_read.decode())
