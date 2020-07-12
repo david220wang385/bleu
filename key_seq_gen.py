5144e6e6a779a302c087ca26c5a570bafa6a4e67
David Wang <david220wang385@gmail.com>
2020-07-11 20:47:40 -0400

    hexify bytes

--- key_seq_gen.py
+++ key_seq_gen.py
@@ -1,26 +1,28 @@
-import hashlib
+import hashlib, binascii
 from hashlib import md5, sha3_384
 """
 References:
 https://www.geeksforgeeks.org/hashlib-sha3_384-in-python/
 https://www.tutorialspoint.com/sha-in-python
-
+https://stackoverflow.com/questions/16862497/python-bytes-literal-has-extra-characters-that-arent-hex-but-alter-the-value-o
 """
 
 def cipher(message, key):
      return bytes([message[i] ^ key[i % len(key)] for i in range(0, len(message))])
 
-key = "tiger"
-msg = "nobody expects the spammish repitition"
+msg = "This is a haiku; it is not too long I think; but you may disagree"
+key = "But there's one sound that no one knows... What does the Fox say?"
 
 # Hash key to create keystream and XOR to encrypt
 # Using hashlib.sha3_384() method 
-keystream = sha3_384() 
-keystream.update(key.encode())
-
-print(keystream.hexdigest().encode())
+# keystream = sha3_384()
+# keystream.update(key.encode())
+# print(keystream.digest())
 
 ciphertext = cipher(msg.encode(), key.encode())
 print(ciphertext)
+print(ciphertext.hex())
+
+# YES JUST WHAT I WANTED
+print(binascii.hexlify(ciphertext))
 cipher_read = bytes(ciphertext)
-print(cipher_read.decode())
