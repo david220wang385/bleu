fd10653a60d4ab0441a91371ff62c602b9eda2e3
David Wang <david220wang385@gmail.com>
2020-07-12 00:13:44 -0400

    Spent a while trying messing around with byte sequence, tl;dr i wasted a lot of time w/o much progress. But now I figured out a way to use n keys to encrypt with SHA3-384

--- key_seq_gen.py
+++ key_seq_gen.py
@@ -5,24 +5,33 @@ References:
 https://www.geeksforgeeks.org/hashlib-sha3_384-in-python/
 https://www.tutorialspoint.com/sha-in-python
 https://stackoverflow.com/questions/16862497/python-bytes-literal-has-extra-characters-that-arent-hex-but-alter-the-value-o
+https://docs.python.org/3.1/library/binascii.html#binascii.b2a_hex
 """
 
 def cipher(message, key):
      return bytes([message[i] ^ key[i % len(key)] for i in range(0, len(message))])
 
 msg = "This is a haiku; it is not too long I think; but you may disagree"
-key = "But there's one sound that no one knows... What does the Fox say?"
+key1 = "But there's one sound that no one knows... What does the Fox say?"
+key2 = "I didn't like that haiku. I should try another one..."
 
 # Hash key to create keystream and XOR to encrypt
 # Using hashlib.sha3_384() method 
-# keystream = sha3_384()
-# keystream.update(key.encode())
-# print(keystream.digest())
+keystream1 = sha3_384()
+keystream1.update(key1.encode())
 
-ciphertext = cipher(msg.encode(), key.encode())
-print(ciphertext)
-print(ciphertext.hex())
+keystream2 = sha3_384()
+keystream2.update(key2.encode())
 
-# YES JUST WHAT I WANTED
-print(binascii.hexlify(ciphertext))
-cipher_read = bytes(ciphertext)
+firstpass = cipher(msg.encode(), keystream1.digest())
+secondpass = cipher(firstpass, keystream2.digest())
+
+firstpassback = cipher(secondpass, keystream2.digest())
+secondpassback = cipher(firstpassback, keystream1.digest())
+
+# Prove cipher works
+print(msg.encode())
+print(firstpass)
+print(secondpass)
+print(firstpassback)
+print(secondpassback)
