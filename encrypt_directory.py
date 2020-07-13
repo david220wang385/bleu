b25daf7953468ae05ddd8e209c1158b9bdfbe40b
David Wang <david220wang385@gmail.com>
2020-07-13 01:36:43 -0400

    Streamlined code for encrypting an entire directory tree; working on argument parsing now

--- encrypt_directory.py
+++ encrypt_directory.py
@@ -7,34 +7,41 @@ https://stackoverflow.com/a/14334768
 https://stackoverflow.com/questions/273192/how-can-i-safely-create-a-nested-directory
 """
 
-import hashlib
+import sys
 from hashlib import sha3_384
-from os import listdir, walk, sep, mkdir
+from os import listdir, walk
 from os.path import isfile, join, splitext
-from pathlib import Path
+
+def main(args):
+
+    # Set variables based on command line arguments
+    start_directory = '../wf_proj_backup/'
+    key = "my_frame_is_the_hand_and_i_am_the_will"
+
+    # Hash key to create keystream to be used in XOR cipher
+    keystream = sha3_384()
+    keystream.update(key.encode())
+
+    # Iterate through all the files in the directory tree (depth-first)
+    for (root, directories, files) in walk(start_directory, topdown=False):
+
+        # Get all files in the current directory
+        file_list = [f for f in listdir(root) if isfile(join(root, f))]
+        
+        for file_ in file_list:
+
+            filepath = join(root, file_)
+            with open(filepath, 'rb+') as file:
+
+                # Read file contents, apply the cipher, and write the result back to the file
+                msg = file.read()
+                ciphertext = cipher(msg, keystream.digest())
+                file.seek(0, 0)
+                file.write(ciphertext)
+
 
 def cipher(message, key):
-     return bytes([message[i] ^ key[i % len(key)] for i in range(0, len(message))])
+    return bytes([message[i] ^ key[i % len(key)] for i in range(0, len(message))])
 
-# Hash key to create keystream and XOR to encrypt
-# Using hashlib.sha3_384() method 
-key = "my_frame_is_the_hand_and_i_am_the_will"
-keystream = sha3_384()
-keystream.update(key.encode())
-
-start_directory = '../wf_proj_backup/'
-for (root, directories, files) in walk(start_directory, topdown=False):
-
-    Path(join(*root.split(sep)[1:])).mkdir(parents=True, exist_ok=True)
-
-    # Get all files in the current directory
-    file_list = [f for f in listdir(root) if (isfile(join(root, f)) and splitext(f)[1]=='.py')]
-
-    for file_ in file_list:
-        with open(join(root, file_), 'rb') as file:
-            msg = file.read()
-            ciphertext = cipher(msg, keystream.digest())
-
-        filename_out = join('.', *root.split(sep)[1:], (splitext(file_)[0] + "_enc" + splitext(file_)[1]))
-        with open(filename_out, 'wb') as file:
-            file.write(ciphertext)
+if __name__ == "__main__":
+   main(sys.argv[1:])
